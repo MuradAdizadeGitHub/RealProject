285dbaa4c38d808c4f4a1b1640e9fd75a47bf846
Zakir Aliyev <aliyevzakir814@gmail.com>
2025-06-06 14:45:37 +0400

    asd

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Helmet } from 'react-helmet-async';
+import {Helmet} from 'react-helmet-async';
+
 function NoPage() {
-  return (
-<>
-<Helmet>
-        <title>alovservis.az | NoPage</title>
-      </Helmet>
-<div>Error 404</div>
-</>
-  )
+    return (
+        <>
+            <Helmet>
+                <title>alovservis.az | NoPage</title>
+            </Helmet>
+            <div style={{
+                height: '500px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>Error 404
+            </div>
+        </>
+    )
 }
 
 export default NoPage
